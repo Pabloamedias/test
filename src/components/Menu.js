efd57f013f0c3de57d7ec0388e831e3be2f49e42
pablinho <p.olmedosaez@gmail.com>
2022-06-05 21:10:08 -0400

    Creamos el menu

---
+++ src/components/Menu.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import "../assets/MenuStyles.css";
+
+const Menu = () => {
+  return (
+    <div>
+      <h1 className="centrado">¡Bienvenida Maritza!</h1>
+      <h2 className="centrado">¿Que necesitas hacer hoy?</h2>
+      <div className="botones">
+        <Button className="button" size="lg">
+          Ingresar pedido
+        </Button>
+        <Button className="button" size="lg">
+          Revisar Historial
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
